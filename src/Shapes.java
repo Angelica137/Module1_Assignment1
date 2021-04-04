f6908339ee1f1cf4c10ff5b26ff5caf393fe7b11
Angelica <Angelica137@users.noreply.github.com>
2021-04-04 20:11:20 +0100

    accept shape from user input

--- src/Shapes.java
+++ src/Shapes.java
@@ -1,6 +1,18 @@
+import java.util.*;
 
 public class Shapes {
+
+	public String inputShape() {
+		// accept user input and returns it
+		Scanner sc = new Scanner(System.in);
+		String shape = sc.nextLine();
+		return shape;
+	}
+
 	public static void main(String[] args) throws Exception {
 		System.out.println("Welcome to shapes. To start playing, enter a shape: ");
+		Shapes s = new Shapes();
+		// s.inputShape();
+		System.out.println(s.inputShape());
 	}
 }
