c61ca74ba6627aa27d0f6a5696fbb8f9e745717f
Angelica <Angelica137@users.noreply.github.com>
2021-04-04 23:56:14 +0100

    draw three required shapes

--- src/Shapes.java
+++ src/Shapes.java
@@ -3,6 +3,8 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.Group;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
 
 public class Shapes extends Application {
@@ -18,6 +20,8 @@ public class Shapes extends Application {
 	@Override
 	public void start(Stage stage) {
 		// create and configure the stage to create a circle
+		// inputShape();
+
 		String shape = inputShape();
 		if (shape.equals("circle")) {
 			Circle circle = new Circle(40, 40, 30);
@@ -28,18 +32,33 @@ public class Shapes extends Application {
 			stage.setScene(scene);
 			stage.show();
 
+		} else if (shape.equals("rectangle")) {
+			Rectangle rectangle = new Rectangle(200, 100);
+			Group root = new Group(rectangle);
+			Scene scene = new Scene(root, 400, 300);
+
+			stage.setTitle("Your shape");
+			stage.setScene(scene);
+			stage.show();
+
+		} else if (shape.equals("octagon")) {
+			Polygon octagon = new Polygon();
+			octagon.getPoints().addAll(new Double[] { 250.0, 50.0, 350.0, 50.0, 400.0, 100.0, 400.0, 200.0, 350.0, 250.0,
+					250.0, 250.0, 200.0, 200.0, 200.0, 100.0 });
+			Group root = new Group(octagon);
+			Scene scene = new Scene(root, 600, 600);
+
+			stage.setTitle("Your shape");
+			stage.setScene(scene);
+			stage.show();
+
 		} else {
 			System.out.println("That is an invalid shape");
 		}
 	}
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 
-		// Shapes s = new Shapes();
-		// test input shape
-		// test .drawShape takes correct input and outputs something
-		// we know .inputShape() works because .drawShape uses it
-		// s.checkInput();
 		launch(args);
 
 	}
