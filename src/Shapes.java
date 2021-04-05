14caf1f856c7fe9ed28a072d7e831d26d10809f4
Angelica <Angelica137@users.noreply.github.com>
2021-04-05 16:35:18 +0100

    remove obsolete import Group

--- src/Shapes.java
+++ src/Shapes.java
@@ -2,7 +2,6 @@
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.Group;
 import javafx.scene.shape.*;
 import javafx.stage.Stage;
 import javafx.scene.control.*;
