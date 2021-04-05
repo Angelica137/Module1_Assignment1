08638d0ba072751aef0098b3fc501cac4b4f12c2
Angelica <Angelica137@users.noreply.github.com>
2021-04-05 13:18:08 +0100

    remove obsolete

--- src/Shapes.java
+++ src/Shapes.java
@@ -18,13 +18,6 @@ import javafx.scene.text.Font;
 
 public class Shapes extends Application {
 
-	/*
-	 * public String inputShape() { // accept user input and returns it
-	 * System.out.println("Welcome to shapes. To start playing, enter a shape: ");
-	 * Scanner sc = new Scanner(System.in); String input = sc.nextLine(); return
-	 * input; }
-	 */
-
 	@Override
 	public void start(Stage stage) {
 
@@ -42,7 +35,7 @@ public class Shapes extends Application {
 		shapeButton.setText("Draw your shape");
 		shapeButton.setOnAction(e -> {
 			// check input and draw shape if input is valid
-			if (shapeField.equals("circle")) {
+			if (shapeField.getText().equals("circle")) {
 				Circle circle = new Circle(40, 40, 30);
 				Group root = new Group(circle);
 				Scene scene = new Scene(root, 400, 300);
@@ -51,7 +44,7 @@ public class Shapes extends Application {
 				stage.setScene(scene);
 				stage.show();
 
-			} else if (shapeField.equals("rectangle")) {
+			} else if (shapeField.getText().equals("rectangle")) {
 				Rectangle rectangle = new Rectangle(200, 100);
 				Group root = new Group(rectangle);
 				Scene scene = new Scene(root, 400, 300);
@@ -60,7 +53,7 @@ public class Shapes extends Application {
 				stage.setScene(scene);
 				stage.show();
 
-			} else if (shapeField.equals("octagon")) {
+			} else if (shapeField.getText().equals("octagon")) {
 				Polygon octagon = new Polygon();
 				octagon.getPoints().addAll(new Double[] { 250.0, 50.0, 350.0, 50.0, 400.0, 100.0, 400.0, 200.0, 350.0, 250.0,
 						250.0, 250.0, 200.0, 200.0, 200.0, 100.0 });
@@ -89,30 +82,6 @@ public class Shapes extends Application {
 		stage.show();
 	}
 
-	/*
-	 * // create and configure the stage to create a circle // inputShape();
-	 * 
-	 * String shape = inputShape(); if (shape.equals("circle")) { Circle circle =
-	 * new Circle(40, 40, 30); Group root = new Group(circle); Scene scene = new
-	 * Scene(root, 400, 300);
-	 * 
-	 * stage.setTitle("Your shape"); stage.setScene(scene); stage.show();
-	 * 
-	 * } else if (shape.equals("rectangle")) { Rectangle rectangle = new
-	 * Rectangle(200, 100); Group root = new Group(rectangle); Scene scene = new
-	 * Scene(root, 400, 300);
-	 * 
-	 * stage.setTitle("Your shape"); stage.setScene(scene); stage.show();
-	 * 
-	 * } else if (shape.equals("octagon")) { Polygon octagon = new Polygon();
-	 * octagon.getPoints().addAll(new Double[] { 250.0, 50.0, 350.0, 50.0, 400.0,
-	 * 100.0, 400.0, 200.0, 350.0, 250.0, 250.0, 250.0, 200.0, 200.0, 200.0, 100.0
-	 * }); Group root = new Group(octagon); Scene scene = new Scene(root, 600, 600);
-	 * 
-	 * stage.setTitle("Your shape"); stage.setScene(scene); stage.show();
-	 * 
-	 * } else { System.out.println("That is an invalid shape"); } }
-	 */
 	public static void main(String[] args) {
 
 		launch(args);
